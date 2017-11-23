8c14bcfc80f47d36025fa6e592b3cc276d132929
Khaled Omar <khaledeomar@gmail.com>
2017-11-23 13:36:22 -0500

    added __doc__ to classes

--- media.py
+++ media.py
@@ -1,9 +1,13 @@
 import webbrowser
 # Video class to created video objects
 class Video:
-    # Video takes in the video title,
-    # description, poster URL, and youtube
-    # trailer URL
+    """    
+    Attributes:
+        video_title (str): Name of the video (e.g. Toy Story)
+        video_discripton (str): General overview of video 
+        video_poster (str): url to image of video poster
+        video_trailer_url (str): url to youtube trailer 
+    """
     def __init__(self,video_title,
                  video_description,
                  video_poster,
@@ -16,6 +20,11 @@ class Video:
 # Movie class inherits from Video class
 # and adds a runtime variable
 class Movie(Video):
+    """
+    Inherits from video
+    Attributes:
+        runtime (int): runtime of the movie in minutes
+    """
     def __init__(self, movie_title,
                  movie_description,
                  movie_poster,
@@ -31,6 +40,11 @@ class Movie(Video):
 # Show class inherits from Video class
 # and adds a number of seasons variable    
 class Show(Video):
+    """
+    Inherits from video
+    Attributes:
+        seasons (int): number of seasons of show
+    """
     def __init__(self, show_title,
                  show_description,
                  show_poster,
